82c87793891359f2acbfa1e8e9bcadcc8d60a3f0
Roman <jrj80mail@gmail.com>
2023-10-16 20:04:26 +0300

    refactoring App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import { PER_PAGE, getImages } from './services/api';
 
@@ -11,110 +11,163 @@ import { Modal } from './Modal/Modal';
 import { Message } from './Message/Message';
 import { Loader } from './Loader/Loader';
 
-export class App extends Component {
-  state = {
-    query: '',
-    image: [],
-    totalImage: 0,
-    page: 1,
-    selectedImageUrl: '',
-    load: false,
-    error: false,
-    loadMore: true,
+// export class App extends Component {
+export const App = () => {
+  // state = {
+  //   query: '',
+  //   image: [],
+  //   totalImage: 0,
+  //   page: 1,
+  //   selectedImageUrl: '',
+  //   load: false,
+  //   error: false,
+  //   loadMore: true,
+  // };
+
+  const [query, setQuery] = useState('');
+  const [image, setImage] = useState([]);
+  const [totalImage, setTotalImage] = useState(0);
+  const [page, setPage] = useState(1);
+  const [selectedImageUrl, setSelectedImageUrl] = useState('');
+  const [load, setLoad] = useState(false);
+  const [error, setError] = useState(false);
+  const [loadMore, setLoadMore] = useState(true);
+
+  //! componentDidUpdate(_, prevState) {
+  //   if (
+  //     prevState.query !== this.state.query ||
+  //     prevState.page !== this.state.page
+  //   ) {
+  //     this.fetchImages();
+  //   }
+  // }
+  // useEffect(() => {
+  //   if (!query) return;
+  // }, [query, page]);
+
+  useEffect(() => {
+    //! `useEffect` has a missing dependency `fetchImages`. Either include it or remove the dependency array
+    //* Оскільки ф-ція 'fetchImages' використовується тільки в 'useEffect()', то перемістимо його сюди:
+    const fetchImages = async () => {
+      try {
+        // this.setState({ load: true, error: false });
+        setLoad(true);
+        setError(false);
+
+        // const response = await getImages(this.state.query, this.state.page);
+        const response = await getImages(query, page);
+
+        // Вивести вміст відповіді на консоль
+        console.log('Вміст відповіді:', response.data);
+
+        // this.setState(prevState => ({
+        //   image: [...prevState.image, ...response.data.hits],
+        //   totalImage: response.data.totalHits,
+        //   loadMore:
+        //     this.state.page < Math.ceil(response.data.totalHits / PER_PAGE),
+        // }));
+
+        setImage(prevImage => [...prevImage, ...response.data.hits]);
+        setTotalImage(response.data.totalHits);
+        setLoadMore(page < Math.ceil(response.data.totalHits / PER_PAGE));
+      } catch (error) {
+        // this.setState({ error: true }); // TODO Інший варіант
+        // this.setState({ error: error.message });
+        setError(error.message);
+      } finally {
+        // this.setState({ load: false });
+        setLoad(false);
+      }
+    };
+
+    // Виклик функції для завантаження зображень при зміні query або page
+    fetchImages();
+  }, [query, page]);
+
+  const getQuery = q => {
+    // this.setState({
+    //   query: `${Date.now()}/${q}`,
+    //   page: 1,
+    //   image: [],
+    //   totalImage: 0,
+    // });
+    setQuery(`${Date.now()}/${q}`);
+    setPage(1);
+    setImage([]);
+    setTotalImage(0);
   };
 
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
-      this.fetchImages();
-    }
-  }
+  // const onBtnClick = () => {
+  //   this.setState(prevState => ({ page: prevState.page + 1 }));
+  // };
 
-  fetchImages = async () => {
-    try {
-      this.setState({ load: true, error: false });
-
-      const response = await getImages(this.state.query, this.state.page);
-
-      // Вивести вміст відповіді на консоль
-      console.log('Вміст відповіді:', response.data);
-
-      this.setState(prevState => ({
-        image: [...prevState.image, ...response.data.hits],
-        totalImage: response.data.totalHits,
-        loadMore:
-          this.state.page < Math.ceil(response.data.totalHits / PER_PAGE),
-      }));
-    } catch (error) {
-      // this.setState({ error: true });
-      this.setState({ error: error.message });
-    } finally {
-      this.setState({ load: false });
-    }
+  const onBtnClick = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  getQuery = q => {
-    this.setState({
-      query: `${Date.now()}/${q}`,
-      page: 1,
-      image: [],
-      totalImage: 0,
-    });
+  const getImageForModal = url => {
+    // this.setState({ selectedImageUrl: url });
+    setSelectedImageUrl(url);
   };
 
-  onBtnClick = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const onModalClose = () => {
+    // this.setState({ selectedImageUrl: '' });
+    setSelectedImageUrl('');
   };
 
-  getImageForModal = url => {
-    this.setState({ selectedImageUrl: url });
-  };
+  // render() {
+  // const showImg = Array.isArray(this.state.image) && this.state.image.length;
+  const showImg = Array.isArray(image) && image.length;
 
-  onModalClose = () => {
-    this.setState({ selectedImageUrl: '' });
-  };
+  return (
+    <Layout>
+      {/* <Searchbar onSubmit={this.getQuery}></Searchbar> */}
+      <Searchbar onSubmit={getQuery}></Searchbar>
+      {showImg && (
+        <ImageGallery
+          // image={this.state.image}
+          // onImageClick={this.getImageForModal}
+          image={image}
+          onImageClick={getImageForModal}
+        ></ImageGallery>
+      )}
 
-  render() {
-    const showImg = Array.isArray(this.state.image) && this.state.image.length;
+      {/* {this.state.load && <Loader></Loader>} */}
+      {load && <Loader></Loader>}
 
-    return (
-      <Layout>
-        <Searchbar onSubmit={this.getQuery}></Searchbar>
-        {showImg && (
-          <ImageGallery
-            image={this.state.image}
-            onImageClick={this.getImageForModal}
-          ></ImageGallery>
-        )}
+      {totalImage > 0 && <div>Total Images: {totalImage}</div>}
 
-        {this.state.load && <Loader></Loader>}
-
-        {/* {this.state.image.length !== 0 &&
+      {/* {this.state.image.length !== 0 &&
           this.state.totalImage > PER_PAGE * this.state.page && (
             <Button onClick={this.onBtnClick}></Button>
           )} */}
 
-        {this.state.image.length !== 0 && this.state.loadMore && (
-          <Button onClick={this.onBtnClick}></Button>
-        )}
+      {/* {this.state.image.length !== 0 && this.state.loadMore && (
+        <Button onClick={this.onBtnClick}></Button>
+      )} */}
+      {image.length !== 0 && loadMore && <Button onClick={onBtnClick}></Button>}
 
-        <Message
-          error={this.state.error}
-          empty={
-            this.state.image.length === 0 &&
-            this.state.query !== '' &&
-            !this.state.load
-          }
-        ></Message>
-        <Modal
-          url={this.state.selectedImageUrl}
-          query={this.state.query}
-          onModalClose={this.onModalClose}
-        ></Modal>
-        <GlobalStyle></GlobalStyle>
-      </Layout>
-    );
-  }
-}
+      <Message
+        // error={this.state.error}
+        error={error}
+        // empty={
+        //   this.state.image.length === 0 &&
+        //   this.state.query !== '' && !this.state.load
+        // }
+        empty={image.length === 0 && query !== '' && !load}
+      ></Message>
+      <Modal
+        // url={this.state.selectedImageUrl}
+        // query={this.state.query}
+        // onModalClose={this.onModalClose}
+        url={selectedImageUrl}
+        query={query}
+        onModalClose={onModalClose}
+      ></Modal>
+      <GlobalStyle></GlobalStyle>
+    </Layout>
+  );
+};
+// }
+
+export default App;
